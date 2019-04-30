13667029e6769d97a2d161908e72eac3ad449947
André Bertolino <andre@agenciatw.com.br>
2019-04-30 19:10:54 -0300

    git clone funcionando perfeitamente no windows

--- bin/wp/clone.sh
+++ bin/wp/clone.sh
@@ -33,20 +33,20 @@ clone()
     rm -rf /c/wamp64/www/$repositorio/wp-content/themes/twentysixteen
     rm -rf /c/wamp64/www/$repositorio/wp-content/themes/twentynineteen
 
-    # # Configurando wordpress
-    # wp core config --dbname=$repositorio --dbhost=localhost --dbuser=root --dbpass=trinity
+    # Configurando wordpress
+    wp core config --dbname=$repositorio --dbhost=localhost --dbuser=root --dbpass=trinity
 
-    # # Bloco de criação do banco
-    # echo ''
-    # echo -e '\033[01;32mCriar Banco de Dados? (y/n)\033[00;37m':
-    # read dbquest
-    # if [ "$dbquest" = "y" ]
-    # then
-    #     echo ''
-    #     mysql -e 'DROP DATABASE IF EXISTS $repositorio;'
+    # Bloco de criação do banco
+    echo ''
+    echo -e '\033[01;32mCriar Banco de Dados? (y/n)\033[00;37m':
+    read dbquest
+    if [ "$dbquest" = "y" ]
+    then
+        echo ''
+        mysql -e 'DROP DATABASE IF EXISTS $repositorio;'
 
-    #     wp db create
-    # fi
+        wp db create
+    fi
     echo ''
     echo -e '\033[01;32mO repositório foi clonado com sucesso!\033[00;37m'
 }
