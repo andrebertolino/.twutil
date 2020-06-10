eb5dec51608c2016821f675bfbc5a0ffbeeaeb71
André Bertolino <olabertolino@gmail.com>
2020-06-09 23:42:20 -0300

    atualizando twp --install

--- bin/wp/install.sh
+++ bin/wp/install.sh
@@ -4,14 +4,14 @@ install()
     echo -e '\033[01;32mProjeto\033[00;37m'
     read projeto
 
-    cd ~/www && mkdir $projeto && cd $projeto  
+    cd $www && mkdir $projeto && cd $projeto  
 
     wp core download --locale=pt_BR
 
     # Configurando wordpress
-    wp core config --dbname=$projeto --dbhost=localhost --dbuser=root --dbpass=trinity
+    wp core config --dbname=$projeto --dbhost=localhost --dbuser=root --dbpass=
 
     # Bloco de criação do banco
     wp db create
-    wp core install --url=http://localhost/$projeto --title=$projeto --admin_user=trinityweb --admin_password=trinity --admin_email=websites@trinityweb.com.br
+    wp core install --url=http://localhost/$projeto --title=$projeto --admin_user=admin --admin_password=root --admin_email=websites@trinityweb.com.br
 }
