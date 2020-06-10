044b7cf61dfdcdeb9814d88fc488d9eba3a545fd
André Bertolino <olabertolino@gmail.com>
2020-06-10 00:14:46 -0300

    Atualizando twp --install teste de diretório

--- bin/wp/install.sh
+++ bin/wp/install.sh
@@ -4,14 +4,20 @@ install()
     echo -e '\033[01;32mProjeto\033[00;37m'
     read projeto
 
-    cd $www && mkdir $projeto && cd $projeto  
+    if [ -d $www/$projeto ]
+    then
+        echo -e '\033[01;33mJá existe um projeto com esse nome, verifique e tente novamente!\033[00;37m'
+        exit
+    else
+        cd $www && mkdir $projeto && cd $projeto 
 
-    wp core download --locale=pt_BR
+        wp core download --locale=pt_BR
 
-    # Configurando wordpress
-    wp core config --dbname=$projeto --dbhost=localhost --dbuser=root --dbpass=
+        # Configurando wordpress
+        wp core config --dbname=$projeto --dbhost=localhost --dbuser=root --dbpass=
 
-    # Bloco de criação do banco
-    wp db create
-    wp core install --url=http://localhost/$projeto --title=$projeto --admin_user=admin --admin_password=root --admin_email=websites@trinityweb.com.br
+        # Bloco de criação do banco
+        wp db create
+        wp core install --url=http://localhost/$projeto --title=$projeto --admin_user=admin --admin_password=root --admin_email=websites@trinityweb.com.br
+    fi
 }
